761ddc5b3b7bcf9f8c83d68e706f21ff38fda5bd
clombardot <cedric.lombardot@gmail.com>
2011-11-10 09:40:41 +0100

    Fix pagination

--- data/generator/sfPropelModule/twitter/template/templates/_pagination.php
+++ data/generator/sfPropelModule/twitter/template/templates/_pagination.php
@@ -1,32 +1,32 @@
 <div class="pagination fRight mts">
     <ul>
-      <li>
+      <li class="prev [?php if (1 == $pager->getPage()): ?]disabled[?php endif; ?]">
         <a href="[?php echo url_for('@<?php echo $this->getUrlForAction('list') ?>') ?]?page=1">
-           [?php echo image_tag(sfConfig::get('sf_admin_module_web_dir').'/images/first.png', array('alt' => __('First page', array(), 'sf_admin'), 'title' => __('First page', array(), 'sf_admin'))) ?]
+           ← First
         </a>
       </li>
 
-    <li>
+    <li class="prev [?php if (1 == $pager->getPage()): ?]disabled[?php endif; ?]">
       <a href="[?php echo url_for('@<?php echo $this->getUrlForAction('list') ?>') ?]?page=[?php echo $pager->getPreviousPage() ?]">
-        [?php echo image_tag(sfConfig::get('sf_admin_module_web_dir').'/images/previous.png', array('alt' => __('Previous page', array(), 'sf_admin'), 'title' => __('Previous page', array(), 'sf_admin'))) ?]
+        ← Previous
       </a>
     </li>
 
       [?php foreach ($pager->getLinks() as $page): ?]
         [?php if ($page == $pager->getPage()): ?]
-          [?php echo $page ?]
+          <li class="active"><a href="#">[?php echo $page ?]</a></li>
         [?php else: ?]
           <li><a href="[?php echo url_for('@<?php echo $this->getUrlForAction('list') ?>') ?]?page=[?php echo $page ?]">[?php echo $page ?]</a></li>
         [?php endif; ?]
       [?php endforeach; ?]
 
-      <li><a href="[?php echo url_for('@<?php echo $this->getUrlForAction('list') ?>') ?]?page=[?php echo $pager->getNextPage() ?]">
-        [?php echo image_tag(sfConfig::get('sf_admin_module_web_dir').'/images/next.png', array('alt' => __('Next page', array(), 'sf_admin'), 'title' => __('Next page', array(), 'sf_admin'))) ?]
+      <li class="next [?php if ($pager->getLastPage() == $pager->getPage()): ?]disabled[?php endif; ?]"><a href="[?php echo url_for('@<?php echo $this->getUrlForAction('list') ?>') ?]?page=[?php echo $pager->getNextPage() ?]">
+        Next →
       </a></li>
 
-      <li>
+      <li class="next [?php if ($pager->getLastPage() == $pager->getPage()): ?]disabled[?php endif; ?]">
       <a href="[?php echo url_for('@<?php echo $this->getUrlForAction('list') ?>') ?]?page=[?php echo $pager->getLastPage() ?]">
-        [?php echo image_tag(sfConfig::get('sf_admin_module_web_dir').'/images/last.png', array('alt' => __('Last page', array(), 'sf_admin'), 'title' => __('Last page', array(), 'sf_admin'))) ?]
+        Last →
       </a>
       </li>
     </ul>
