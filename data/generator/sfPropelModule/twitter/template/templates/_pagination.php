13208415bc3ead5811e1a41da0c21a656d16d5ec
Jeremy <jeremy.benoist@gmail.com>
2011-11-19 00:34:02 +0100

    Better pagination
    Few css fixes for the pagination.
    Remove text and add double arrow.

--- data/generator/sfPropelModule/twitter/template/templates/_pagination.php
+++ data/generator/sfPropelModule/twitter/template/templates/_pagination.php
@@ -1,33 +1,27 @@
-<div class="pagination fRight mts">
-    <ul>
-      <li class="prev [?php if (1 == $pager->getPage()): ?]disabled[?php endif; ?]">
-        <a href="[?php echo url_for('@<?php echo $this->getUrlForAction('list') ?>') ?]?page=1">
-           ← First
-        </a>
-      </li>
 
+
+<div class="pagination fRight mts">
+  <ul>
+    <li class="first [?php if (1 == $pager->getPage()): ?]disabled[?php endif; ?]">
+      <a title="[?php echo __('First page', array(), 'sf_admin') ?]" href="[?php echo url_for('@<?php echo $this->getUrlForAction('list') ?>') ?]?page=1">&lArr;</a>
+    </li>
     <li class="prev [?php if (1 == $pager->getPage()): ?]disabled[?php endif; ?]">
-      <a href="[?php echo url_for('@<?php echo $this->getUrlForAction('list') ?>') ?]?page=[?php echo $pager->getPreviousPage() ?]">
-        ← Previous
-      </a>
+      <a title="[?php echo __('Previous page', array(), 'sf_admin') ?]" href="[?php echo url_for('@<?php echo $this->getUrlForAction('list') ?>') ?]?page=[?php echo $pager->getPreviousPage() ?]">&larr;</a>
     </li>
 
-      [?php foreach ($pager->getLinks() as $page): ?]
-        [?php if ($page == $pager->getPage()): ?]
-          <li class="active"><a href="#">[?php echo $page ?]</a></li>
-        [?php else: ?]
-          <li><a href="[?php echo url_for('@<?php echo $this->getUrlForAction('list') ?>') ?]?page=[?php echo $page ?]">[?php echo $page ?]</a></li>
-        [?php endif; ?]
-      [?php endforeach; ?]
+    [?php foreach ($pager->getLinks() as $page): ?]
+      [?php if ($page == $pager->getPage()): ?]
+        <li class="active"><a href="#">[?php echo $page ?]</a></li>
+      [?php else: ?]
+        <li><a href="[?php echo url_for('@<?php echo $this->getUrlForAction('list') ?>') ?]?page=[?php echo $page ?]">[?php echo $page ?]</a></li>
+      [?php endif; ?]
+    [?php endforeach; ?]
 
-      <li class="next [?php if ($pager->getLastPage() == $pager->getPage()): ?]disabled[?php endif; ?]"><a href="[?php echo url_for('@<?php echo $this->getUrlForAction('list') ?>') ?]?page=[?php echo $pager->getNextPage() ?]">
-        Next →
-      </a></li>
-
-      <li class="next [?php if ($pager->getLastPage() == $pager->getPage()): ?]disabled[?php endif; ?]">
-      <a href="[?php echo url_for('@<?php echo $this->getUrlForAction('list') ?>') ?]?page=[?php echo $pager->getLastPage() ?]">
-        Last →
-      </a>
-      </li>
-    </ul>
+    <li class="next [?php if ($pager->getLastPage() == $pager->getPage()): ?]disabled[?php endif; ?]">
+      <a title="[?php echo __('Next page', array(), 'sf_admin') ?]" href="[?php echo url_for('@<?php echo $this->getUrlForAction('list') ?>') ?]?page=[?php echo $pager->getNextPage() ?]">&rarr;</a>
+    </li>
+    <li class="last [?php if ($pager->getLastPage() == $pager->getPage()): ?]disabled[?php endif; ?]">
+      <a title="[?php echo __('Last page', array(), 'sf_admin') ?]" href="[?php echo url_for('@<?php echo $this->getUrlForAction('list') ?>') ?]?page=[?php echo $pager->getLastPage() ?]">&rArr;</a>
+    </li>
+  </ul>
 </div>
