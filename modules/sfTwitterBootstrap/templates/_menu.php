0f8ebb97ff0541c6939628e93a11b06aacda4282
Florian <fpondepeyre@gmail.com>
2011-12-08 12:06:52 +0100

    Added the possibility to create a menu which combined simple item and dropdown menu.
    Configure your app.yml just like this with the "url" key under your master level:
    
    categories:
      "Products":
        url: @product
      "Users":
        items:
          "Settings":
          url: @settings
          image: settings.png

--- modules/sfTwitterBootstrap/templates/_menu.php
+++ modules/sfTwitterBootstrap/templates/_menu.php
@@ -1,38 +1,60 @@
 <?php
-  use_helper('I18N');
-  /** @var Array of menu items */ $items = $sf_data->getRaw('items');
-  /** @var Array of categories, each containing an array of menu items and settings */ $categories = $sf_data->getRaw('categories');
+use_helper('I18N');
+/** @var Array of menu items */ $items = $sf_data->getRaw('items');
+/** @var Array of categories, each containing an array of menu items and settings */ $categories = $sf_data->getRaw('categories');
 ?>
 
 
 <?php if (count($items)): ?>
   <ul class="nav">
     <?php if (sfTwitterBootstrap::hasItemsMenu($items)): ?>
-    <li class="dropdown" data-dropdown="dropdown"><a href="#" class="dropdown-toggle" >Menu</a>
-      <ul class="dropdown-menu">
-        <?php include_partial('sfTwitterBootstrap/menu_list', array('items' => $items, 'items_in_menu' => true)); ?>
-      </ul>
-    </li>
-    <?php  endif; ?>
+      <li class="dropdown" data-dropdown="dropdown"><a href="#" class="dropdown-toggle" >Menu</a>
+        <ul class="dropdown-menu">
+          <?php include_partial('sfTwitterBootstrap/menu_list', array('items' => $items, 'items_in_menu' => true)); ?>
+        </ul>
+      </li>
+    <?php endif; ?>
     <?php include_partial('sfTwitterBootstrap/menu_list', array('items' => $items, 'items_in_menu' => false)); ?>
   </ul>
 <?php endif; ?>
+
 <?php if (count($categories)): ?>
   <ul class="nav">
+
     <?php foreach ($categories as $name => $category): ?>
-    <?php   if (sfTwitterBootstrap::hasPermission($category, $sf_user)): ?>
-    <?php     if (sfTwitterBootstrap::hasItemsMenu($category['items'])): ?>
-    <li class="dropdown"><a href="#" class="dropdown-toggle" ><?php echo __(isset($category['name']) ? $category['name'] : $name) ?></a>
-      <ul class="dropdown-menu">
-        <?php include_partial('sfTwitterBootstrap/menu_list', array('items' => $category['items'], 'items_in_menu' => true)) ?>
-      </ul>
-    </li>
-    <?php     endif; ?>
-    <?php   endif; ?>
+      <?php if (sfTwitterBootstrap::hasPermission($category, $sf_user)): ?>
+        <li class="dropdown">
+
+          <?php if (isset($category['url'])): ?>
+
+            <?php $class = ''; ?>
+            <?php if (isset($category['items'])): ?>
+              <?php if (sfTwitterBootstrap::hasItemsMenu($category['items'])): ?>
+                <?php $class = 'dropdown-toggle'; ?>
+              <?php endif; ?>
+            <?php endif; ?>
+
+          <a href="<?php echo url_for($category['url']); ?>" class="<?php echo $class; ?>" ><?php echo __(isset($category['name']) ? $category['name'] : $name) ?></a>
+
+          <?php else: ?>
+            <a href="#" class="dropdown-toggle" ><?php echo __(isset($category['name']) ? $category['name'] : $name) ?></a>
+          <?php endif; ?>
+
+          <?php if (isset($category['items']) && sfTwitterBootstrap::hasItemsMenu($category['items'])): ?>
+            <ul class="dropdown-menu">
+              <?php include_partial('sfTwitterBootstrap/menu_list', array('items' => $category['items'], 'items_in_menu' => true)) ?>
+            </ul>
+          <?php endif; ?>
+        </li>
+      <?php endif; ?>
     <?php endforeach; ?>
+
     <?php foreach ($categories as $name => $category): ?>
+      <?php if (isset($category['items'])): ?>
         <?php include_partial('sfTwitterBootstrap/menu_list', array('items' => $category['items'], 'items_in_menu' => false)) ?>
+      <?php endif; ?>
     <?php endforeach; ?>
+
   </ul>
 <?php elseif (!count($items)): ?>
   <?php echo __('sfTwitterBootstrap is not configured.'); ?>
